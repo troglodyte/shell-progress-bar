1ad372d3e49b628f8b92c8a1ae3e5178a6c683ae
mharris <throwingfish@gmail.com>
2025-11-10 13:19:37 -0500

    removing unused tracer bullet code

--- barable.sh
+++ barable.sh
@@ -26,21 +26,3 @@ getPercent() {
   local result=$((current * 100 / total))
   echo "$result"
 }
-
-#
-#echo 'Starting...'
-#files=(./**/*cache*)
-#len=${#files[@]}
-#echo "Found $len files"
-
-#tput civis # hide cursor
-#fileCount=1 # Start at 1
-#for file in "${files[@]}"; do
-#        makeBar $fileCount $len
-#        sleep .001
-#        ((fileCount++))
-#done;
-#tput cnorm # show cursor
-
-#
-#echo -e "$(makeBar 40 100)";
